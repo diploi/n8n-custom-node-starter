415814d42cfffedba86a44bf35897bfb85304df7
giaongo <83873333+giaongo@users.noreply.github.com>
2026-01-03 23:28:00 +0200

    Implement working n8n-server, node file-watching

--- n8n-script.sh
+++ n8n-script.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
-# Exit immediately if a command fails
-set -e
+echo "Watching dist/ for changes..."
 
-echo "Building and linking custom node..."
-
-npm run build
-
-npm link
-
-mkdir -p n8n-server/.n8n/custom 
-
-cd n8n-server/.n8n/custom
-
-npm link n8n-nodes-nasapics
-
-echo "Custom node linked successfully!"
-
-echo "Starting n8n server..."
-
-n8n start
+npx nodemon \
+  --watch /app/nodes/ \
+  --watch /app/credentials/ \
+  --ext ts,js,json \
+  --exec "sh ./restart-n8n.sh"
 
 
 
---
+++ restart-n8n.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Exit immediately if a command fails
+set -e
+N8N_PID=""
+
+start_n8n() {
+    echo "Starting n8n..."
+    n8n start &
+    N8N_PID=$!
+    echo "n8n started (PID $N8N_PID)"
+}
+
+stop_n8n() {
+    if [ -n "$N8N_PID" ]; then
+        echo "Stopping n8n (PID $N8N_PID)..."
+        kill -SIGINT "$N8N_PID"
+        wait "$N8N_PID" || true
+        echo "n8n stopped."
+    fi
+}
+
+build_n8n_node() {
+    echo "Building and linking custom node..."
+
+    npm run build
+
+    cp -rf ./nodes ./n8n-server/.n8n/custom/nodes
+
+    cp -rf ./credentials ./n8n-server/.n8n/custom/credentials
+
+    echo "Custom node built successfully!"
+}
+
+# Stop n8n if running
+stop_n8n
+
+# Build and link the custom node
+build_n8n_node
+
+# Start n8n server
+start_n8n
+
+# trap to ensure n8n is stopped on script exit
+trap "stop_n8n" EXIT
